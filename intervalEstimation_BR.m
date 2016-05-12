b65ece7e824d8cd6cc2f1a07eedcb8cd9f52188f
Rodarkh <rodrigo.d.1990@gmail.com>
2016-05-12 14:13:34 +0100

    Version with time distribution creatiion
    now we are basicaly missing the PTB code.

--- intervalEstimation_BR.m
+++ intervalEstimation_BR.m
@@ -18,27 +18,33 @@ elseif strcmp(modality,'Auditory') || strcmp(modality,'auditory')
     isvisual =0;
     modality_text = 'auditory';
 else
-    error('Modality unknown, the program only has Visual and Auditory modalities')
+    error('Modality unknown, the program only has Visual and Auditory modalities.')
 end
 
 if strcmp(interval_type,'Long') || strcmp(interval_type,'long')
-    isvisual =1;
+    islong =1;
     interval_type_text = 'long';
 elseif strcmp(interval_type,'Short') || strcmp(interval_type,'short')
-    isvisual =0;
+    islong =0;
     interval_type_text = 'short';
 else
-    error('Modality unknown, the program only has Visual and Auditory modalities')
+    error('Time Interval Distribution unknown, the program only has Short and Long distributions.')
 end
 
 n_trials = 10;
 
 
 %% Time intervals definitions
+pre_stim_dist = 1000; % Let us use a gaussion distribution around the mean
 
-short_time_def = [500 850];
-long_time_def = [850 1200];
+short_time_def = [500 850]; %ms
+long_time_def = [850 1200]; %ms
 
+if islong
+   curr_time_dist =  long_time_def;
+elseif ~islong
+   curr_time_dist =  short_time_def;
+end
 
 %% PsychToolbox initializations
 
@@ -48,9 +54,9 @@ long_time_def = [850 1200];
 %% Trial starts
 
 for trl = 1:n_trials
-    curr
-    
-    
+    curr_pre_stim = pre_stim_dist + 100*randn(1);
+    curr_time = curr_time_dist(1) + (curr_time_dist(2)-curr_time_dist(1))*rand(1);
+ 
     %% Auditory Block
     if ~isvisual
         
@@ -75,9 +81,12 @@ data.estimate = zeros(n_trials,1);
 data.pre_stim = zeros(n_trials,1);
 data.time = zeros(n_trials,1);
 
+
 data.pre_stim(trl) = curr_pre_stim;
 data.time(trl) = curr_time;
 data.estimate(trl) = curr_estimate;
+data.time_dist = curr_time_dist;
+
 %% Trial end
 end
 
